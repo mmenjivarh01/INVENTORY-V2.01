04fbc5a6a4290a820a20f753a6531471cd82ad83
Juan Manuel Menjivar <156940570+mmenjivarh01@users.noreply.github.com>
2026-06-10 01:45:28 -0400

    v2.1.58

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ export const firebaseConfig = {
 export const APP = {
   inventoryKey: "a",
   defaultLanguage: "en",
-  version: "2.1.54",
+  version: "2.1.58",
   brand: "Afghan Kabob & Grill"
 };
 
